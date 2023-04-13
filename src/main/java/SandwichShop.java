308759ace463e639a97fab816999d9ceaf401d8e
10zinnyims <tnyima@appdev.yearup.org>
2023-04-13 14:19:26 -0400

    added files

---
+++ src/main/java/SandwichShop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        double RegularPrice = 5.45;
+        double LargePrice = 8.95;
+        double StudentDis = 0.10;
+        double SeniorDis = 0.20;
+// Student 17 or younger gets 10% discount
+// Senior 65 or older gets 20% discount
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter sandwich size Regular or Large: ");
+        String size = scanner.next();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        double price = (size = LargePrice : RegularPrice);
+
+        if (age <= 17) {
+            price =
+        }
+    }
+}
